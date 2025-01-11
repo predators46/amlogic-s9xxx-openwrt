c1948b1a4db1fe615cc8a43d6f7620e90755a8d8
Predators <31931530+predators46@users.noreply.github.com>
2025-01-11 20:53:31 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -186,7 +186,7 @@ rebuild_firmware() {
 	\
         bash perl perl-http-date perlbase-file perlbase-getopt perlbase-time perlbase-unicode perlbase-utf8 \
 	\
-	dnsmasq-full \
+	dnsmasq-full luci-app-passwall2 \
 	\
 	dnsmasq-full ipset iptables iptables-mod-conntrack-extra iptables-mod-iprange iptables-mod-tproxy kmod-ipt-nat \
 	\
