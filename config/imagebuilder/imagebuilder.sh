c312e0bb3555bc35913ce807a6bd995c7566ab09
Predators <31931530+predators46@users.noreply.github.com>
2025-05-02 21:21:39 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -200,7 +200,7 @@ rebuild_firmware() {
     
     mkdir openwrt
     #wget https://github.com/predators46/hack/releases/download/18.06.4/openwrt-18.06.4-armvirt-64-default-rootfs.tar.gz
-    tar xvf openwrt-21.02.7-armvirt-64-default-rootfs.tar.gz -C openwrt
+    tar xvf openwrt-23.05.5-armsr-armv8-generic-rootfs.tar.gz -C openwrt
     
     wget https://github.com/predators46/amlogic-s9xxx-openwrt/releases/download/OpenWrt_imagebuilder__2025.05/openwrt_amlogic_s905x_k5.15.180_2025.05.02.img.gz
     gunzip openwrt_amlogic_s905x_k5.15.180_2025.05.02.img.gz
