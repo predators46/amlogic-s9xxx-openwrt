f3d1cf8c53c2a4f1837dc7fd1b518fbedec2d94c
Predators <31931530+predators46@users.noreply.github.com>
2025-05-02 21:19:37 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -182,7 +182,7 @@ rebuild_firmware() {
         \
         mariadb-client-extra mariadb-server-extra \
         \
-        luci-nginx \
+        luci-nginx nginx-ssl-util \
         \
         dnsmasq-full \
         \
@@ -195,6 +195,38 @@ rebuild_firmware() {
 
     # Rebuild firmware
     make image PROFILE="" PACKAGES="${my_packages}" FILES="files"
+    
+    cd bin/targets/*/*/
+    
+    mkdir openwrt
+    #wget https://github.com/predators46/hack/releases/download/18.06.4/openwrt-18.06.4-armvirt-64-default-rootfs.tar.gz
+    tar xvf openwrt-21.02.7-armvirt-64-default-rootfs.tar.gz -C openwrt
+    
+    wget https://github.com/predators46/amlogic-s9xxx-openwrt/releases/download/OpenWrt_imagebuilder__2025.05/openwrt_amlogic_s905x_k5.15.180_2025.05.02.img.gz
+    gunzip openwrt_amlogic_s905x_k5.15.180_2025.05.02.img.gz
+    mkdir armbian
+    sudo losetup -P -f --show openwrt_amlogic_s905x_k5.15.180_2025.05.02.img
+    ls /dev/loop3*
+    sudo mount /dev/loop3p2 armbian
+    
+    sudo rm -rf openwrt/lib/firmware
+    sudo rm -rf openwrt/lib/modules
+    
+    sudo mv armbian/lib/modules openwrt/lib/
+    sudo mv armbian/lib/firmware openwrt/lib/
+
+    #sudo sed -i '/kmodloader/i \\tulimit -n 51200\n' openwrt/etc/init.d/boot
+    
+    sudo rm -rf armbian/*
+    sudo mv openwrt/* armbian/
+    sudo mkdir armbian/boot
+    sudo sync
+    sudo umount armbian
+    sudo losetup -d /dev/loop3
+    
+    xz --compress openwrt_amlogic_s905x_k5.15.180_2025.05.02.img
+    
+    cd ${imagebuilder_path}
 
     sync && sleep 3
     echo -e "${INFO} [ ${openwrt_dir}/bin/targets/*/* ] directory status: $(ls bin/targets/*/* -al 2>/dev/null)"
