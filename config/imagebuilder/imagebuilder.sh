bd42194148aa99c837d6ab56625e2a2bbac92c46
Predators <31931530+predators46@users.noreply.github.com>
2025-05-13 09:49:31 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -120,7 +120,7 @@ custom_packages() {
     echo -e "${STEPS} Start adding custom packages..."
 
     # Clone [ packages ] directory
-    rm -rf packages && git clone -b 21 "https://github.com/esaaprillia/packages"
+    #rm -rf packages && git clone -b 21 "https://github.com/esaaprillia/packages"
     #git clone "https://github.com/esaaprillia/package" && cp -r package/* packages/ && rm -rf package
     [[ "${?}" -eq "0" ]] || error_msg "[ packages ] clone failed!"
     echo -e "${INFO} The [ packages ] is clone successfully."
