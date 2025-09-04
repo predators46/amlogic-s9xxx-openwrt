b191b342eb2eb439975f2cba80067371a42c02b5
Predators <31931530+predators46@users.noreply.github.com>
2025-09-04 19:30:47 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -190,36 +190,14 @@ rebuild_firmware() {
     
     cd bin/targets/*/*/
     
-    sudo mkdir openwrt
-    #wget https://github.com/predators46/hack/releases/download/18.06.4/openwrt-18.06.4-armvirt-64-default-rootfs.tar.gz
-    sudo tar xvf openwrt-24.10.2-armsr-armv8-generic-rootfs.tar.gz -C openwrt
-    
-    sudo wget https://github.com/predators46/amlogic-s9xxx-openwrt/releases/download/OpenWrt_imagebuilder__2025.08/openwrt_amlogic_s905x_k6.6.102_2025.08.25.img.gz
-    sudo gunzip openwrt_amlogic_s905x_k6.6.102_2025.08.25.img.gz
-    sudo mkdir armbian
-    sudo losetup -P -f --show openwrt_amlogic_s905x_k6.6.102_2025.08.25.img
-    sudo ls /dev/loop3*
-    sudo mount /dev/loop3p2 armbian
-    
-    sudo rm -rf openwrt/lib/firmware
-    sudo rm -rf openwrt/lib/modules
-    
-    sudo mv armbian/lib/modules openwrt/lib/
-    sudo mv armbian/lib/firmware openwrt/lib/
+    git clone https://git.openwrt.org/openwrt/openwrt.git && cd openwrt
+    git checkout v24.10.2
+    ./scripts/feeds update -a && ./scripts/feeds install -a
+    wget -O package/libs/toolchain/Makefile https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/24.10.2/Makefile
+    wget -O .config https://downloads.openwrt.org/releases/24.10.2/targets/armsr/armv8/config.buildinfo
+    make defconfig
+    make -j$(nproc)
 
-    sudo sed -i '/kmodloader/i \\tulimit -n 51200\n' openwrt/etc/init.d/boot
-    
-    sudo rm -rf armbian/*
-    sudo rm -rf armbian/.reserved
-    sudo rm -rf armbian/.snapshots
-    sudo mv openwrt/* armbian/
-    sudo mkdir armbian/boot
-    sudo sync
-    sudo umount armbian
-    sudo losetup -d /dev/loop3
-    
-    sudo xz --compress openwrt_amlogic_s905x_k6.6.102_2025.08.25.img
-    
     cd ${imagebuilder_path}
 
     sync && sleep 3
