dc5a3e768e156cc590633fdaf9a8320aaf473f94
Predators <31931530+predators46@users.noreply.github.com>
2025-09-05 20:12:25 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -195,7 +195,7 @@ rebuild_firmware() {
     ./scripts/feeds update -a && ./scripts/feeds install -a
     #wget -O package/libs/toolchain/Makefile https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/24.10.2/Makefile
     wget -O toolchain/gcc/Config.in https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/24.10.2/Config.in
-    wget -O .config https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/24.10.2/config.buildinfo
+    wget -O .config https://downloads.openwrt.org/releases/24.10.2/targets/armsr/armv8/config.buildinfo
     make defconfig
     make -j$(nproc)
     zip -r bin.zip bin
