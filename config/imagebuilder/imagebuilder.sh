de13e7db3703846a14c759dbac3eeb02f86ff756
Predators <31931530+predators46@users.noreply.github.com>
2025-09-05 16:39:41 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -194,6 +194,7 @@ rebuild_firmware() {
     git checkout v24.10.2
     ./scripts/feeds update -a && ./scripts/feeds install -a
     #wget -O package/libs/toolchain/Makefile https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/24.10.2/Makefile
+    wget -O toolchain/gcc/Config.in https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/24.10.2/Config.in
     wget -O .config https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/24.10.2/config.buildinfo
     make defconfig
     make -j$(nproc)
