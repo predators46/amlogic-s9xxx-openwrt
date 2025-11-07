8c9f2203175048e47f51b1eabd6641c4abfbb545
Predators <31931530+predators46@users.noreply.github.com>
2025-11-07 22:16:16 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -174,7 +174,7 @@ rebuild_firmware() {
         \
         -dnsmasq -procd-ujail \
         \
-        kmod-fs-vfat lsblk btrfs-progs uuidgen dosfstools tar fdisk wpad-basic-mbedtls wifi-scripts iw iwinfo kmod-cfg80211 kmod-mac80211 \
+        kmod-fs-vfat lsblk btrfs-progs uuidgen dosfstools tar fdisk wpad-basic-mbedtls iw iwinfo kmod-cfg80211 kmod-mac80211 \
         \
         ${config_list} \
         "
