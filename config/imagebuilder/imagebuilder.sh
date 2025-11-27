7e8f3a75ebe74470fbffcc74d63afed8151562e0
Predators <31931530+predators46@users.noreply.github.com>
2025-11-27 08:25:51 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -114,7 +114,7 @@ custom_packages() {
 
     # Download [ packages ] directory
     #rm -rf packages && git clone -b 24 "https://github.com/esaaprillia/packages"
-    rm -rf packages && wget https://github.com/firmwarecostum/mosdns/releases/download/909-1/mosdns_ipk_ARMSR.zip
+    mkdir -p packages && wget https://github.com/firmwarecostum/mosdns/releases/download/909-1/mosdns_ipk_ARMSR.zip
     unzip mosdns_ipk_ARMSR.zip && cp -r bin/packages/aarch64_generic/python/* packages/ && rm -rf bin
     wget https://github.com/esaaprillia/packages/raw/refs/heads/ha/libgfortran_13.3.0-r4_aarch64_generic.ipk && cp -r libgfortran_13.3.0-r4_aarch64_generic.ipk packages/
     [[ "${?}" -eq "0" ]] || error_msg "[ packages ] download failed!"
