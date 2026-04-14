89ef6a6d4a6e905d13b8030b9476c6d456206970
Predators <31931530+predators46@users.noreply.github.com>
2026-04-14 16:31:35 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -217,6 +217,7 @@ rebuild_firmware() {
     wget -O .config https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/25.12.0/glibc/config.buildinfo
     wget -O feeds/packages/lang/perl/files/libc.config https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/25.12.0/glibc/libc.config
     wget -O feeds/packages/lang/perl/files/base.config https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/25.12.0/glibc/base.config
+    wget -O feeds/packages/lang/perl/patches/902-ext_POSIX_POSIX.patch https://raw.githubusercontent.com/esaaprillia/br/refs/heads/main/25.12.0/glibc/902-ext_POSIX_POSIX.patch
     make defconfig
     make -j$(nproc) V=s
     zip -r bin.zip bin
