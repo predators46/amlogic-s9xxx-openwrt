5e2bbdcf6d4670c7502807afef705bab8ff8971b
Predators <31931530+predators46@users.noreply.github.com>
2026-04-15 19:31:30 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -193,7 +193,7 @@ rebuild_firmware() {
         \
         -dnsmasq \
         \
-        sudo bash wget-ssl coreutils-whoami coreutils-fold coreutils-hostname \
+        sudo bash wget-ssl coreutils-whoami coreutils-fold coreutils-hostname libstdcpp \
         \
         kmod-fs-vfat lsblk btrfs-progs uuidgen dosfstools tar fdisk \
         \
